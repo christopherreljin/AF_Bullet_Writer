af59c81e8fe24279f948d9c19e11b6c4ff2d1b9f
christopherreljin <75033039+christopherreljin@users.noreply.github.com>
2020-11-25 08:53:01 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -105,24 +105,20 @@ try {
   if (localStorage.getItem('bullet-settings')) {
     settings = JSON.parse(localStorage.getItem("bullet-settings"));
 
-
   }
 } catch (err) {
   if (err.name === 'SecurityError') {
-    console.log('Was not able to get localstorage bullets due to use of file interface and browser privacy settings');
+    console.log('Unable to get localstorage bullets due to use of file interface and browser privacy settings.');
   } else {
     throw err;
   }
 }
-
-
 
 WebFont.load({
     custom: {
         families: ['AdobeTimes']
     }
 });
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -131,41 +127,16 @@ ReactDOM.render(
     </div>
     <div className="container" id="footer">
       <div>If you have feedback, submit
-          an <a href='https://github.com/AF-VCD/pdf-bullets/issues'>issue</a>
-          or a <a href="https://github.com/AF-VCD/pdf-bullets/pulls">pull request</a>
+          an <a href="https://github.com/christopherreljin/AF_Bullet_Writer/issues">issue</a> 
+          or a <a href="https://github.com/christopherreljin/AF_Bullet_Writer/pulls">pull request</a>.
       </div>
       <div>This site utilizes PDF.JS (pdf import), HandsOnTable (spreadsheet), the DataMuse API (thesaurus), and Bulma (CSS).</div>
-      <div>This site has basic analytics to track the total number of visits to the page. See <a href="https://github.com/ckhordiasma/log-bullet-visitors">here</a> for details</div>
-      <div>Maintained by Christopher Kodama </div>
+      <div>This Folk is maintained by <a href='mailto:christopher.reljin@us.af.mil'>Christopher Reljin</a></div>
     </div>
   </React.StrictMode>
 , document.getElementById('root'));
-
-incrementVisitors();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
-
-function incrementVisitors() {
-  // code for incrementing visitor count    
-  const xh = new XMLHttpRequest();
-  xh.onreadystatechange = () => {  
-    if(xh.readyState === XMLHttpRequest.DONE) {
-      var status = xh.status;
-
-      // Not sure what status codes are acceptable.. using MDN successful responses and redirects range as a guide
-      if ((status >= 200 && status < 400)) {
-          // The request has been completed successfully
-          const count = JSON.parse(xh.response).Count;
-          console.log("The bullets site(s) have been visited " + count + " times.");
-
-      } else {
-        console.log("Visitor count increment: task failed successfully");
-      }
-    }
-  }
-  xh.open("POST", "https://g5z50elklh.execute-api.us-east-2.amazonaws.com/default/LogVisitors",true); 
-  xh.send();
-}
